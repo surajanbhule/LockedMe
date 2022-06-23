9aab39df842a6b91c39be59e2a738e852bce1a04
surajanbhule <surajanbhule69@gmail.com>
2022-06-23 18:38:16 +0530

    Welcome Screen & How to use guide added ======================================= Application details and app guide added. Sprint1 >> Task1:Welcome Screen completed.

--- LockedMe/src/classes/Util.java
+++ LockedMe/src/classes/Util.java
@@ -20,8 +20,28 @@ class Util {
 		System.out.println("Project Description:-");
 		
 		System.out.println("          This application allow you to create files \n and perform  basic operations on that file.");
+		printLine();
+		animationEffect();
+		System.out.println("       ********** > How to use < **********");
+		printLine();
+		appGuide();
+		printLine();
 	}
 	
+	private static void appGuide() {
+		// TODO Auto-generated method stub
+		System.out.println("1. Read the instructions at end of the console.");
+		animationEffect();
+		System.out.println("2. Provide correct input as per the instructions.");
+		animationEffect();
+		System.out.println("3. If wrong input provided, you will get one more try.");
+		animationEffect();
+		System.out.println("4. You will get menu number to select perticular menu item.");
+		animationEffect();
+		System.out.println("5. Thank you, explore the app.");
+		
+	}
+
 	private static void animationEffect() {
 		try {
 			Thread.sleep(700);
